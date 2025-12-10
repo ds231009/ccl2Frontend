2387ce6a4c9cfd232f6bb2f3bd5e6fe72f0a436f
ds231009 <ds231009@fhstp.ac.at>
2025-12-10 14:57:27 +0100

    Change vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  // base: '', // <- YOUR repo name
   base: '/ccl2Frontend/', // <- YOUR repo name
   plugins: [react()]
 })
